0a4f7336e2eddf0a0de4cfb8ca58a218007ddf83
timkoeppel <Tim.Koeppel1@gmail.com>
2021-10-12 18:19:37 +0200

    Fixed Camera angles for #2. Device orientation camera still not working

--- src/game.ts
+++ src/game.ts
@@ -8,7 +8,7 @@ export default class Game {
     private _canvas: HTMLCanvasElement;
     private _engine: BABYLON.Engine;
     private _scene: BABYLON.Scene;
-    private _camera: BABYLON.ArcRotateCamera;
+    private _camera: BABYLON.DeviceOrientationCamera;
     private _light: BABYLON.Light;
     private _vr: BABYLON.VRExperienceHelper;
 
@@ -21,31 +21,35 @@ export default class Game {
         // Lights
         this._light = new BABYLON.HemisphericLight(
             "main_light", 
-            new BABYLON.Vector3(0, 0, 0), 
+            new BABYLON.Vector3(0, 50, 0), 
             this._scene
         );
-
-        // Camera
-        this._camera = new BABYLON.ArcRotateCamera(
-            "camera_white",
-            0,1,1,
-            new BABYLON.Vector3(0, 5, -10), 
-            this._scene
-        );
-        this._camera.setTarget(BABYLON.Vector3.Zero());
-        this._camera.attachControl(this._canvas, false);
 
         // Load Scene
-        BABYLON.SceneLoader.AppendAsync("", "scene.babylon", this._scene).then(result => {
-            //camera.setTarget(scene.getMeshByName("king_b"));
-
+        BABYLON.SceneLoader.ImportMeshAsync("", "", "scene.babylon", this._scene).then(result => {
+            // Board adjustment due to import error
+            result.meshes[1].position = new BABYLON.Vector3(0,25,0);
+            result.meshes[2].position = new BABYLON.Vector3(0,25,0);
         });
 
         // Enable VR
         this._vr = this._scene.createDefaultVRExperience({
             createDeviceOrientationCamera: false,
-            useXR: true
+            useXR: false,
         });
+
+        // Camera
+        this._camera = new BABYLON.DeviceOrientationCamera(
+            "camera_white",
+            new BABYLON.Vector3(0, 40, 15), 
+            this._scene
+        );
+        this._scene.activeCamera = this._camera;
+        this._camera.setTarget(BABYLON.Vector3.Zero());
+        this._camera.attachControl(this._canvas, true);
+        //this._camera.angularSensibility = 10000;
+        //this._camera.disablePointerInputWhenUsingDeviceOrientation = true;
+        
     }
 
     public DoRender(): void {
